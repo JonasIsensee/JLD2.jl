92bba7f766daa22333086cabf29c1d93af09a17b
Jonas Isensee <jonas.isensee@web.de>
2020-07-05 09:23:39 +0200

    Previous check was too careful. Disable only readmode "r"

--- src/JLD2.jl
+++ src/JLD2.jl
@@ -202,8 +202,8 @@ function jldopen(fname::AbstractString, wr::Bool, create::Bool, truncate::Bool, 
                  compress::Bool=false, mmaparrays::Bool=false) where T<:Union{Type{IOStream},Type{MmapIO}}
     exists = isfile(fname)
     if exists
-        if filesize(fname) == 0
-            throw(ArgumentError("File exists but is empty"))
+        if filesize(fname) == 0 && (wr, create, truncate) == (false, false, false)
+            throw(ArgumentError("Cannot read from an empty file"))
         end
         rname = realpath(fname)
         if haskey(OPEN_FILES, rname)
