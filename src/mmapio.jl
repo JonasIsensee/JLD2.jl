fa72e168c396e2316e992eb990a4d7f0e7702828
Jonas Isensee <jonas.isensee@ds.mpg.de>
2020-07-10 11:12:27 +0200

    always grow by exact amount that is needed

--- src/mmapio.jl
+++ src/mmapio.jl
@@ -135,8 +135,10 @@ function Base.resize!(io::MmapIO, newend::Ptr{Cvoid})
 
     # Resize file
     ptr = io.startptr
+    #newsz = Int(max(newend - ptr, io.curptr - ptr + FILE_GROW_SIZE))
     #newsz = Int(max(newend - ptr + 102, io.curptr - ptr + FILE_GROW_SIZE))
-    newsz = Int(max(newend - ptr, io.curptr - ptr)) + FILE_GROW_SIZE
+    #newsz = Int(max(newend - ptr, io.curptr - ptr)) + FILE_GROW_SIZE
+    newsz = Int(newend - ptr)
     @static if !Sys.iswindows()
         grow(io.f, newsz)
     end
